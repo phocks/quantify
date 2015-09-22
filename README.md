143eb553e55fe3e0403418dcade30296325001b6
Joshua Byrd <phocks@gmail.com>
2015-09-22 23:09:38 +1000

    dns test and readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # quantify
 experiment with quotes
+## Installing and running
+Clone into directory.
+Run using `node main.js`
+Visit with web browser.
--- main.js
+++ main.js
@@ -6,3 +6,9 @@ http.createServer(function (req, res) {
 }).listen(1337, "127.0.0.1");
 
 console.log('Server running at http://127.0.0.1:1337/');
+
+var dns = require('dns');
+
+dns.lookup('www.google.com', function onLookup(err, addresses, family) {
+  console.log('addresses:', addresses);
+});
