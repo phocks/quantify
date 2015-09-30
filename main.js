ae86245fbf4d83ad1a7d1c0ebbcecd3aea16ecb0
Joshua Byrd <phocks@gmail.com>
2015-09-30 21:09:04 +1000

    adding package.json

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-var http = require('http');
+// var http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, "127.0.0.1");
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World\n');
+// }).listen(1337, "127.0.0.1");
 
-console.log('Server running at http://127.0.0.1:1337/');
+// console.log('Server running at http://127.0.0.1:1337/');
 
-var dns = require('dns');
+// var dns = require('dns');
 
-dns.lookup('www.google.com', function onLookup(err, addresses, family) {
-  console.log('addresses:', addresses);
-});
+// dns.lookup('www.google.com', function onLookup(err, addresses, family) {
+//   console.log('addresses:', addresses);
+// });
+
+var express = require('express')
+var app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(1337)
